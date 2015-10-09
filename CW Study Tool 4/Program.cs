27e87fea5dd628185bce3ed0e4fec3ceb19927cd
陈为 <chenwei2000121@qq.com>
2015-10-09 16:35:46 +0800

    add support for high dpi!

--- CW Study Tool 4/Program.cs
+++ CW Study Tool 4/Program.cs
@@ -14,9 +14,14 @@ namespace CW_Study_Tool_4
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
