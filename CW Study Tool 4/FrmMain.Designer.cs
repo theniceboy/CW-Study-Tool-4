45147c48747a0fa53127c1ef763eef31747ac255
陈为 <chenwei2000121@qq.com>
2015-10-10 14:40:16 +0800

    tmp

--- CW Study Tool 4/FrmMain.Designer.cs
+++ CW Study Tool 4/FrmMain.Designer.cs
@@ -29,141 +29,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.btnNewWordsWalkthough = new DevComponents.DotNetBar.ButtonX();
             this.styleManager1 = new DevComponents.DotNetBar.StyleManager(this.components);
-            this.btnMulltiLearn = new DevComponents.DotNetBar.ButtonX();
-            this.btnMultiReview = new DevComponents.DotNetBar.ButtonX();
-            this.btnReviewWords = new DevComponents.DotNetBar.ButtonX();
-            this.btnImport = new DevComponents.DotNetBar.ButtonX();
-            this.btnWordList = new DevComponents.DotNetBar.ButtonX();
-            this.btnHelp = new DevComponents.DotNetBar.ButtonX();
             this.SuspendLayout();
-            // 
-            // btnNewWordsWalkthough
-            // 
-            this.btnNewWordsWalkthough.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnNewWordsWalkthough.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnNewWordsWalkthough.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnNewWordsWalkthough.FocusCuesEnabled = false;
-            this.btnNewWordsWalkthough.Location = new System.Drawing.Point(12, 33);
-            this.btnNewWordsWalkthough.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnNewWordsWalkthough.Name = "btnNewWordsWalkthough";
-            this.btnNewWordsWalkthough.Size = new System.Drawing.Size(550, 95);
-            this.btnNewWordsWalkthough.TabIndex = 0;
-            this.btnNewWordsWalkthough.Text = "New Words Walkthough";
-            this.btnNewWordsWalkthough.Click += new System.EventHandler(this.btnNewWordsWalkthough_Click);
             // 
             // styleManager1
             // 
             this.styleManager1.ManagerStyle = DevComponents.DotNetBar.eStyle.Office2016;
             this.styleManager1.MetroColorParameters = new DevComponents.DotNetBar.Metro.ColorTables.MetroColorGeneratorParameters(System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255))))), System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(115)))), ((int)(((byte)(199))))));
             // 
-            // btnMulltiLearn
-            // 
-            this.btnMulltiLearn.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnMulltiLearn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMulltiLearn.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnMulltiLearn.FocusCuesEnabled = false;
-            this.btnMulltiLearn.Location = new System.Drawing.Point(12, 154);
-            this.btnMulltiLearn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnMulltiLearn.Name = "btnMulltiLearn";
-            this.btnMulltiLearn.Size = new System.Drawing.Size(550, 95);
-            this.btnMulltiLearn.TabIndex = 1;
-            this.btnMulltiLearn.Text = "Multi-Round Learn";
-            this.btnMulltiLearn.Click += new System.EventHandler(this.btnMulltiLearn_Click);
-            // 
-            // btnMultiReview
-            // 
-            this.btnMultiReview.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnMultiReview.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnMultiReview.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnMultiReview.FocusCuesEnabled = false;
-            this.btnMultiReview.Location = new System.Drawing.Point(12, 396);
-            this.btnMultiReview.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnMultiReview.Name = "btnMultiReview";
-            this.btnMultiReview.Size = new System.Drawing.Size(550, 95);
-            this.btnMultiReview.TabIndex = 2;
-            this.btnMultiReview.Text = "Multi-Round Review";
-            this.btnMultiReview.Click += new System.EventHandler(this.btnMultiReview_Click);
-            // 
-            // btnReviewWords
-            // 
-            this.btnReviewWords.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnReviewWords.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnReviewWords.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnReviewWords.FocusCuesEnabled = false;
-            this.btnReviewWords.Location = new System.Drawing.Point(12, 275);
-            this.btnReviewWords.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnReviewWords.Name = "btnReviewWords";
-            this.btnReviewWords.Size = new System.Drawing.Size(550, 95);
-            this.btnReviewWords.TabIndex = 3;
-            this.btnReviewWords.Text = "Review Words";
-            this.btnReviewWords.Click += new System.EventHandler(this.btnReviewWords_Click);
-            // 
-            // btnImport
-            // 
-            this.btnImport.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnImport.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnImport.FocusCuesEnabled = false;
-            this.btnImport.Location = new System.Drawing.Point(12, 517);
-            this.btnImport.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(550, 95);
-            this.btnImport.TabIndex = 4;
-            this.btnImport.Text = "Import New Words";
-            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
-            // 
-            // btnWordList
-            // 
-            this.btnWordList.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnWordList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnWordList.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnWordList.FocusCuesEnabled = false;
-            this.btnWordList.Location = new System.Drawing.Point(12, 638);
-            this.btnWordList.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnWordList.Name = "btnWordList";
-            this.btnWordList.Size = new System.Drawing.Size(550, 95);
-            this.btnWordList.TabIndex = 5;
-            this.btnWordList.Text = "Word List";
-            this.btnWordList.Click += new System.EventHandler(this.btnWordList_Click);
-            // 
-            // btnHelp
-            // 
-            this.btnHelp.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.btnHelp.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnHelp.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.btnHelp.FocusCuesEnabled = false;
-            this.btnHelp.Location = new System.Drawing.Point(12, 759);
-            this.btnHelp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnHelp.Name = "btnHelp";
-            this.btnHelp.Size = new System.Drawing.Size(550, 95);
-            this.btnHelp.TabIndex = 6;
-            this.btnHelp.Text = "Help";
-            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
-            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(574, 871);
-            this.Controls.Add(this.btnHelp);
-            this.Controls.Add(this.btnWordList);
-            this.Controls.Add(this.btnImport);
-            this.Controls.Add(this.btnReviewWords);
-            this.Controls.Add(this.btnMultiReview);
-            this.Controls.Add(this.btnMulltiLearn);
-            this.Controls.Add(this.btnNewWordsWalkthough);
+            this.ClientSize = new System.Drawing.Size(1261, 808);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ForeColor = System.Drawing.Color.Black;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "FrmMain";
@@ -175,14 +57,6 @@
         }
 
         #endregion
-
-        private DevComponents.DotNetBar.ButtonX btnNewWordsWalkthough;
         private DevComponents.DotNetBar.StyleManager styleManager1;
-        private DevComponents.DotNetBar.ButtonX btnMulltiLearn;
-        private DevComponents.DotNetBar.ButtonX btnMultiReview;
-        private DevComponents.DotNetBar.ButtonX btnReviewWords;
-        private DevComponents.DotNetBar.ButtonX btnImport;
-        private DevComponents.DotNetBar.ButtonX btnWordList;
-        private DevComponents.DotNetBar.ButtonX btnHelp;
     }
 }
--- CW Study Tool 4/FrmMain.cs
+++ CW Study Tool 4/FrmMain.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevComponents.DotNetBar.Metro;
+using MySql.Data.MySqlClient;
 
 namespace CW_Study_Tool_4
 {
@@ -20,7 +21,23 @@ namespace CW_Study_Tool_4
 
         private void FrmMain_Load(object sender, EventArgs e)
         {
+            String myInfo = "欢迎使用 ASP.NET MVC! ";
 
+            ///////////////////获取MYSQ看数据返回值////////////////////////////  
+            MySqlConnection myconn = null;
+            MySqlCommand mycom = null;
+            MySqlDataReader myrec = null;
+
+            //连接字符串拼装  
+            myconn = new MySqlConnection("Host = 127.0.0.1;Database = test;Username = root;Password = fulei");
+
+            //连接  
+            myconn.Open();
+
+            if (myconn.State.ToString() == "Open")
+            {
+                MessageBox.Show("连接MYSQL成功");
+            }
         }
 
         private void btnNewWordsWalkthough_Click(object sender, EventArgs e)
